2bd039d11dc9a70406fd9d5cd1be86658c2aff95
Hendy Irawan <ceefour666@gmail.com>
2015-01-09 01:35:19 +0000

    Imported from Pak Marzuki's, with Emgu CV files moved to project directory.

---
+++ FewDoubleCamera/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace FewDoubleCamera
+{
+    partial class FormDuble
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.iB = new Emgu.CV.UI.ImageBox();
+            this.cBKamera = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lt = new System.Windows.Forms.Label();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.btnAmbil = new System.Windows.Forms.Button();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSimpan = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.iB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // iB
+            // 
+            this.iB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.iB.Location = new System.Drawing.Point(0, 0);
+            this.iB.Name = "iB";
+            this.iB.Size = new System.Drawing.Size(344, 285);
+            this.iB.TabIndex = 2;
+            this.iB.TabStop = false;
+            // 
+            // cBKamera
+            // 
+            this.cBKamera.FormattingEnabled = true;
+            this.cBKamera.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.cBKamera.Location = new System.Drawing.Point(223, 293);
+            this.cBKamera.Name = "cBKamera";
+            this.cBKamera.Size = new System.Drawing.Size(121, 21);
+            this.cBKamera.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PilihKamera Yang Akan Digunakan";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(38, 324);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(170, 23);
+            this.bStart.TabIndex = 5;
+            this.bStart.Text = "Start Deteksi";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(370, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Total Terdeteksi";
+            // 
+            // lt
+            // 
+            this.lt.AutoSize = true;
+            this.lt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lt.Location = new System.Drawing.Point(391, 44);
+            this.lt.Name = "lt";
+            this.lt.Size = new System.Drawing.Size(16, 16);
+            this.lt.TabIndex = 7;
+            this.lt.Text = "0";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(360, 63);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(150, 150);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // btnAmbil
+            // 
+            this.btnAmbil.Location = new System.Drawing.Point(360, 219);
+            this.btnAmbil.Name = "btnAmbil";
+            this.btnAmbil.Size = new System.Drawing.Size(150, 23);
+            this.btnAmbil.TabIndex = 8;
+            this.btnAmbil.Text = "Ambil Sample";
+            this.btnAmbil.UseVisualStyleBackColor = true;
+            this.btnAmbil.Click += new System.EventHandler(this.btnAmbil_Click);
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(556, 76);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(161, 20);
+            this.textBoxNama.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(556, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nama Dari Wajah Sampe";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // buttonSimpan
+            // 
+            this.buttonSimpan.Location = new System.Drawing.Point(556, 102);
+            this.buttonSimpan.Name = "buttonSimpan";
+            this.buttonSimpan.Size = new System.Drawing.Size(161, 23);
+            this.buttonSimpan.TabIndex = 11;
+            this.buttonSimpan.Text = "Simpan Data Pelatihan";
+            this.buttonSimpan.UseVisualStyleBackColor = true;
+            this.buttonSimpan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormDuble
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 372);
+            this.Controls.Add(this.buttonSimpan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxNama);
+            this.Controls.Add(this.btnAmbil);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.lt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cBKamera);
+            this.Controls.Add(this.iB);
+            this.Name = "FormDuble";
+            this.Text = "DubleKamera";
+            this.Load += new System.EventHandler(this.FormDuble_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.iB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox iB;
+        private System.Windows.Forms.ComboBox cBKamera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lt;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.Button btnAmbil;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSimpan;
+    }
+}
+
---
+++ FewDoubleCamera/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+namespace FewDoubleCamera
+{
+    public partial class FormDuble : Form
+    {
+        private Capture capture;
+        private bool progres;
+        private HaarCascade haar; // Detector menggunakan Viola=John clasifier
+       
+
+        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5d, 0.5d);
+        Image<Gray, byte> result = null, TrainedFace = null;
+        Image<Gray, byte> gray = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> NamePersons = new List<string>();
+        int ContTrain, NumLabels, t;
+        string name, names = null;
+        public FormDuble()
+        {
+            InitializeComponent();
+            this.Text = "Pengenalan Wajah";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void prossesFrame(object sender, EventArgs args)
+        {
+
+            Image<Bgr, Byte> imageFrame = capture.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC); 
+            if (imageFrame != null)
+            {
+                Image<Gray, byte> grayframe = imageFrame.Convert<Gray, byte>();
+                var faces = grayframe.DetectHaarCascade(haar, 1.1, 1,
+                                        HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                                        new Size(20, 20))[0];
+                int TotalFaces = faces.Length;   
+                lt.Text = Convert.ToString(TotalFaces);
+              
+
+                foreach (var face in faces)
+                {
+                    t = t + 1;
+                    result = imageFrame.Copy(face.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                    imageFrame.Draw(face.rect, new Bgr(Color.Red), 2);
+                     if (trainingImages.ToArray().Length != 0)
+                    {
+                        MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+                        EigenObjectRecognizer recognizer = new EigenObjectRecognizer(
+                        trainingImages.ToArray(),
+                        labels.ToArray(),
+                        3000,
+                        ref termCrit);
+                        name = recognizer.Recognize(result);             
+                        imageFrame.Draw(name, ref font, new Point(face.rect.X - 2, face.rect.Y - 2), new Bgr(Color.Yellow));
+                    }
+                   // NamePersons[t-1] = name;
+                   // NamePersons.Add("");
+                    //Set the number of faces detected on the scene
+                    //label3.Text = faces.Length.ToString();
+                }
+                /*t = 0;
+                for (int nnn = 0; nnn < faces.Length; nnn++)
+                {
+                        names = names + NamePersons[nnn] + ", ";
+                }*/
+                   iB.Image = imageFrame;
+                  // names = "";
+                    //Clear the list(vector) of names
+                  NamePersons.Clear();
+            }
+        }
+
+
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            ambilData();
+            int number = 1;
+            number = int.Parse(cBKamera.Text);
+            #region
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new Capture(number);
+                }
+                catch (NullReferenceException exp)
+                {
+                    MessageBox.Show(exp.Message);
+                }
+            }
+            #endregion
+            if (capture != null)
+            {
+                if (progres)
+                {
+                    bStart.Text = "Start Deteksi";
+                    Application.Idle -= prossesFrame;
+
+                }
+                else
+                {
+                    bStart.Text = "Stop";
+                    Application.Idle += prossesFrame;
+                }
+                progres = !progres;
+                
+            }
+        }
+
+        private void FormDuble_Load(object sender, EventArgs e)
+        {
+            haar = new HaarCascade("haarcascade_frontalface_alt_tree.xml");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAmbil_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+               
+                ContTrain = ContTrain + 1;
+                gray = capture.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Image<Gray, byte> grayframe = gray.Convert<Gray, byte>();
+                var faces = grayframe.DetectHaarCascade(haar, 1.1, 1,
+                                        HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                                        new Size(20, 20))[0];
+                
+                foreach (var face in faces)
+                {
+                    TrainedFace = gray.Copy(face.rect).Convert<Gray, byte>();
+                    break;
+                }
+                TrainedFace=result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                imageBox1.Size = TrainedFace.Size;
+                imageBox1.Image = TrainedFace;
+            }
+            catch
+            {
+                //Jika terjadi kesalahan
+            }
+            //trainingImages.Add(TrainedFace);
+            imageBox1.Image = TrainedFace;
+        }
+
+
+        private void ambilData()
+        {
+            try
+            {
+                string Labelsinfo = File.ReadAllText(Application.StartupPath + "/wajah/namaTrain.txt");
+                string[] Labels = Labelsinfo.Split('%');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                ContTrain = NumLabels;
+                string filewajah;
+
+                for (int no = 1; no < NumLabels + 1; no++)
+                {
+                    filewajah = "wajah" + no + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/wajah/" + filewajah));
+                    labels.Add(Labels[no]);
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Data Pelatihan tidak ditemukan"+"\n"+"Lajutkan dengan pembuatan data pelatihan", "Load Wajah Pelatihan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxNama.Text == "")
+                MessageBox.Show("Nama Pemilik Gambar Harus Terisi", "Data Pelatihan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            else
+            {
+               // TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                trainingImages.Add(TrainedFace);
+                labels.Add(textBoxNama.Text);
+
+                try{
+                    File.WriteAllText(Application.StartupPath + "/wajah/namaTrain.txt", trainingImages.ToArray().Length.ToString() + "%");
+                for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                {
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/wajah/wajah" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/wajah/namaTrain.txt", labels.ToArray()[i - 1] + "%");
+                }
+
+                MessageBox.Show("Wajah "+textBoxNama.Text + " telah dideteksi dan ditambahkan pada Data Training", "Proses Data Training ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Keslahan Proses Training", "Kesalahan Training", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            }
+        }
+    }
+}
